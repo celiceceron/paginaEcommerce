9da8fe5c2005912039f8004c1958150e949a18a7
celiceceron <celice.cer@gmail.com>
2022-12-06 17:41:42 +0100

    ajustando o codigo

--- HTML/assets/java.js
+++ HTML/assets/java.js
@@ -1,6 +1,6 @@
-const createProductButton = document.querySelector('#create-product')
-const dialog = document.querySelector('.create-product_dialog')
-const cancelDialogButton = document.querySelector('#cancel')
+const createProductButton = document.querySelector("#create-product");
+const dialog = document.querySelector(".create-product_dialog");
+const cancelDialogButton = document.querySelector("#cancel");
 
 // dialog.open = true funciona
 // dialog.open = false funciona
@@ -12,16 +12,27 @@ const cancelDialogButton = document.querySelector('#cancel')
 // dialog.showModal() // funciona;
 // dialog.close() // funciona
 
-createProductButton.addEventListener('click', openModal);
+createProductButton.addEventListener("click", openModal);
 
 function openModal() {
-    dialog.showModal();
+  dialog.showModal();
 }
 
-cancelDialogButton.addEventListener('click', () => {
-    dialog.close();
+cancelDialogButton.addEventListener("click", () => {
+  dialog.close(); 
 });
+
+//// DICA: CRIAR NOVAS FUNCOES PARA RESPONSABILIDADES DIFERENTES.
 
 const inputs = document.querySelectorAll('input');
 const saveButton = document.querySelector('#save');
-console.log('saveButton', saveButton);
+saveButton.addEventListener('click', getInputValues);
+
+function getInputValues(){
+    const inputName = inputs[0].value;
+    const inputPrice = inputs[1].value;
+    const inputImage = inputs[2].value;
+    const inputImageDescription = inputs[3].value;
+    const productDescription = document.querySelector('#description').value;
+console.log(inputName, inputPrice, inputImage, inputImageDescription, productDescription);
+};
