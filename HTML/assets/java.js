2c314e1b144e58aa78f04d8bca1898dd301191f5
celiceceron <celice.cer@gmail.com>
2022-12-07 20:05:09 +0100

    criando elementos com DOM

--- HTML/assets/java.js
+++ HTML/assets/java.js
@@ -1,6 +1,6 @@
-const createProductButton = document.querySelector(`#create-product`);
-const dialog = document.querySelector(`.create-product_dialog`);
-const cancelDialogButton = document.querySelector(`#cancel`);
+const createProductButton = document.querySelector('#create-product')
+const dialog = document.querySelector('.create-product-dialog')
+const cancelDialogButton = document.querySelector('#cancel')
 
 // dialog.open = true funciona
 // dialog.open = false funciona
@@ -12,40 +12,53 @@ const cancelDialogButton = document.querySelector(`#cancel`);
 // dialog.showModal() // funciona;
 // dialog.close() // funciona
 
-createProductButton.addEventListener(`click`, openModal);
+createProductButton.addEventListener('click', openModal);
 
 function openModal() {
     dialog.showModal();
 }
 
-cancelDialogButton.addEventListener(`click`, () => {
+cancelDialogButton.addEventListener('click', () => {
     dialog.close();
-});
+})
 
 //// DICA: CRIAR NOVAS FUNCOES PARA RESPONSABILIDADES DIFERENTES.
 
 const inputs = document.querySelectorAll('input');
 const saveButton = document.querySelector('#save');
-saveButton.addEventListener('click', getInputValues);
+console.log('saveButton', saveButton );
 
-function getInputValues() {
-    const inputName = inputs[0].value;
-    const inputPrice = inputs[1].value;
-    const inputImage = inputs[2].value;
-    const inputImageDescription = inputs[3].value;
-    const productDescription = document.querySelector('#description').value;
+
+//PASSEI O CODIGO PRA BAIXO
+//saveButton.addEventListener('click', getInputValues);
+
+//RESP. DA FUNCAO (PEGAR VALORES)
+//function getInputValues() {
+    //const inputName = inputs[0].value;
+    //const inputPrice = inputs[1].value;
+    //const inputImage = inputs[2].value;
+    //const inputImageDescription = inputs[3].value;
+    //const productDescription = document.querySelector('#description').value;
     //console.log(inputName, inputPrice, inputImage, inputImageDescription, productDescription);
 
     ///CRIANDO UM OBJETO COM AS INFORMACOES:
 
-    console.log({
-        inputName,
-        inputPrice,
-        inputImage,
-        inputImageDescription,
-        productDescription,
-    })
-};
+    //console.log({
+        //inputName,
+        //inputImageDescription,
+        //inputPrice,
+       // inputImage,
+       // productDescription,
+    //})
+
+    //return {
+      //  inputName,
+       // inputImageDescription,
+       // inputPrice,
+      //  inputImage,
+       // productDescription,
+   // }
+//};
 
 ///PRIMEIRA MANEIRA DE CRIR UM NOVO PRODUCT CARD:
 //const productCard = document.createElement(`article`);
@@ -67,36 +80,30 @@ function getInputValues() {
 //INNERHTML SOBSCREVE TUDO QUE VEIO ANTES. SE USAR += NO FINAL ELE FAZ APARECER O QUE FOI CRIADO ANTES TB.
 
 //productCard.innerHTML =
-`<article class="product-card">
-<img alt="Um conjunto de martelo, alicate, trena e chaves dentro de uma caixa preta"
-  src="./assets/produto-kit-ferramentas.jpg" class="product-card__product-image"></img>
-<hr>
-</hr>
-<h1 class="product-card__product-price">R$ 128,00</h1>
-<h2 class="product-card__product-name">Jogo de Ferramentas Kit 129 Peças C/ Maleta + 40 Peças</h2>
-<p class="product-card__product-description">Esse é um jogo de peças extremamente útil para diversas
-  atividades, visto que o conjunto de peças é muito variado.</p>`
 
 
-  //CRIANDO A FUNCAO (COPIEI OS COMANDOS DE CIMA)
-function createProductCard() {
-    const productCard = document.createElement('article');
-    productCard.classList.add('product-card');
-    const productList = document.querySelector('.product-list');
 
-    productCard.innerHTML =
-        `<article class="product-card">
-<img alt="Um conjunto de martelo, alicate, trena e chaves dentro de uma caixa preta"
-  src="./assets/produto-kit-ferramentas.jpg" class="product-card__product-image"></img>
-<hr>
-</hr>
-<h1 class="product-card__product-price">R$ 128,00</h1>
-<h2 class="product-card__product-name">Jogo de Ferramentas Kit 129 Peças C/ Maleta + 40 Peças</h2>
-<p class="product-card__product-description">Esse é um jogo de peças extremamente útil para diversas
-  atividades, visto que o conjunto de peças é muito variado.</p>`
-}
+//CRIANDO A FUNCAO (COPIEI OS COMANDOS DE CIMA)
+//RESP. DA FUNCAO (CRIAR PRODUTO)
+//function createProductCard() {
+   //const productCard = document.createElement('article');
+    //productCard.classList.add('product-card');
+    //const productList = document.querySelector('.product-list');
 
-//COLOCANDO O PRODUT CARD DENTRO DO PRODUCT LIST
-productList.appendChild(productCard);
+    //productCard.innerHTML =
+   
 
 
+    //COLOCANDO O PRODUT CARD DENTRO DO PRODUCT LIST
+   // productList.appendChild(productCard);
+//}
+
+//FUNCAO PARA SALVAR E CAPTURAR INFORMACOES:
+//function handleClickSave() {
+    //getInputValues primeira função que pega valores
+    //const productValues = getInputValues()
+    //console.log('productValues', productValues)
+
+//}
+
+//saveButton.addEventListener('click', handleClickSave);
