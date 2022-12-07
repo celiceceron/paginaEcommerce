ba0b1a875a5d1351a014c5318cbcb3832c256f28
celiceceron <celice.cer@gmail.com>
2022-12-07 20:38:32 +0100

    criando elementos com DOM

--- HTML/assets/java.js
+++ HTML/assets/java.js
@@ -18,6 +18,33 @@ function openModal() {
   dialog.showModal();
 }
 
+
+//DICA_PARA CADA FUNCIONALIDADE CRIAR UMA NOVA FUNÇÃO.
 cancelDialogButton.addEventListener('click', () => {
   dialog.close();
 })
+
+const inputs = document.querySelectorAll('input');
+const saveButton = document.querySelector('#save');
+saveButton.addEventListener('click', getInputValues);
+
+//FUNÇÃO QUE CAPTURA VALORES.
+function getInputValues(){
+    const inputName = inputs[0].value;
+    const inputPrice = inputs[1].value;
+    const inputImage = inputs[2].value;
+    const inputImageDescription = inputs[3].value;
+    const productDescription = document.querySelector('#description').value;
+    console.log({
+        inputName, 
+        inputPrice, 
+        inputImage, 
+        inputImageDescription, 
+        productDescription,
+    })
+}
+
+//FUNÇÃO PARA CRIAR ELEMENTOS.
+const productCard = document.createElement('article')
+productCard.classList.add('product-card')
+console.log(productCard)
