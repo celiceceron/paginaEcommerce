d36814a0c34dceca16003faa05ccb7b262e8f1c7
celiceceron <celice.cer@gmail.com>
2022-12-07 20:57:41 +0100

    criando elementos com DOM

--- HTML/assets/java.js
+++ HTML/assets/java.js
@@ -45,12 +45,28 @@ function getInputValues(){
 }
 
 //FUNÇÃO PARA CRIAR ELEMENTOS.
+//PRIMEIRA MANEIRA DE CRIAR ELEMENTO.
+//onst productCard = document.createElement('article')
+//productCard.classList.add('product-card')
+//const h2 = document.createElement('h2')
+//productCard.appendChild(h2)
+//h2.textContent = 'Jogo de Ferramentas'
+//PRODUCT LIST É O PAI
+//const productList = document.querySelector('.product-list')
+//productList.appendChild(productCard)
+//console.log(productList)
+
+//SEGUNDA E MELHOR MANEIRA DE CRIAR ELEMENTO.
 const productCard = document.createElement('article')
 productCard.classList.add('product-card')
-const h2 = document.createElement('h2')
-productCard.appendChild(h2)
-h2.textContent = 'Jogo de Ferramentas'
-//PRODUCT LIST É O PAI
 const productList = document.querySelector('.product-list')
+productCard.innerHTML = 
+`<img alt="Um conjunto de martelo, alicate, trena e chaves dentro de uma caixa preta"
+src="./assets/produto-kit-ferramentas.jpg" class="product-card__product-image"></img>
+<hr>
+</hr>
+<h1 class="product-card__product-price">R$ 128,00</h1>
+<h2 class="product-card__product-name">Jogo de Ferramentas Kit 129 Peças C/ Maleta + 40 Peças</h2>
+<p class="product-card__product-description">Esse é um jogo de peças extremamente útil para diversas
+atividades, visto que o conjunto de peças é muito variado.</p>`
 productList.appendChild(productCard)
-console.log(productList)
